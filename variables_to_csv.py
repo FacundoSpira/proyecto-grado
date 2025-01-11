2957179e67087bfe84a344b2a4eb643ca53ed68a
Facundo Spira <facundospira@gmail.com>
2025-01-11 16:11:02 -0300

    feat: improve schedule creation by not requiring to pass turns
    This makes it more modular, since we can now use it outside of the
    context of the data itself

--- variables_to_csv.py
+++ variables_to_csv.py
@@ -1,16 +1,23 @@
 import csv
 
 
-def create_schedule_csv(variables, turns, csv_name="schedule.csv"):
+def create_schedule_csv(variables, csv_name="schedule.csv"):
     # Nos quedamos solo con las variables x que valen 1.
     schedule_vars = {}
     for v in variables:
         if v.name.startswith("x_") and v.value() == 1.0:
             schedule_vars[v.name] = v.value()
 
+    # Extraemos los días y turnos máximos de las variables
+    max_day = 1
+    max_turn = 1
+    for var_name in schedule_vars:
+        parts = var_name.split("_")
+        _, _, day, turn = parts
+        max_day = max(max_day, int(day))
+        max_turn = max(max_turn, int(turn))
+
     # Creamos un diccionario con la estructura de la tabla.
-    max_day = max(turns.keys())
-    max_turn = max(max(turn_list) for turn_list in turns.values())
     schedule = {
         i: {j: [] for j in range(1, max_turn + 1)} for i in range(1, max_day + 1)
     }
@@ -35,21 +42,9 @@ def create_schedule_csv(variables, turns, csv_name="schedule.csv"):
         for day in range(1, max_day + 1):
             row = [f"Día {day}"]
 
-            turns_list = turns[day] if day in turns else None
-
-            # Si el día no está en la lista, deshabilitamos todos los turnos.
-            if turns_list is None:
-                row += ["-" for _ in range(max_turn)]
-                writer.writerow(row)
-                continue
-
-            # Si el día está en la lista, procesamos cada turno.
+            # Procesamos cada turno
             for turn in range(1, max_turn + 1):
-                if turn in turns_list:
-                    cell = ", ".join(schedule[day][turn]) if schedule[day][turn] else ""
-                else:
-                    cell = "-"  # Marca turno deshabilitado
-
+                cell = ", ".join(schedule[day][turn]) if schedule[day][turn] else ""
                 row.append(cell)
 
             writer.writerow(row)
