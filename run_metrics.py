8544efba27c8250007b9680aa5688ad31e18a007
Facundo Spira <facundospira@gmail.com>
2025-03-30 10:31:30 -0300

    feat: some runs for metrics

--- run_metrics.py
+++ run_metrics.py
@@ -1,17 +1,19 @@
 from metrics import generate_metrics
+import sys
 
 DATA_PATH = "data"
 CASE_PATH = "casos/caso_lg"
 
-metrics = generate_metrics(
-    # "output/schedule_8hs-alpha:1-beta:1-nuevas-restricciones.csv",
-    # "output/schedule_nuevo-15m-alpha:1-beta:1.csv",
-    # "output/schedule_nuevo-15m-alpha:1-beta:0.csv",
-    "ema_schedule.csv",
-    f"{DATA_PATH}/unidades_curriculares.csv",
-    f"{CASE_PATH}/coincidencia.csv",
-    f"{DATA_PATH}/previas.csv",
-    f"{CASE_PATH}/trayectoria_sugerida.csv",
-)
 
-print(metrics)
+if __name__ == "__main__":
+    schedule_file = sys.argv[1] if len(sys.argv) > 1 else "ema_schedule.csv"
+
+    metrics = generate_metrics(
+        schedule_file,
+        f"{DATA_PATH}/unidades_curriculares.csv",
+        f"{CASE_PATH}/coincidencia.csv",
+        f"{DATA_PATH}/previas.csv",
+        f"{CASE_PATH}/trayectoria_sugerida.csv",
+    )
+
+    print(metrics)
